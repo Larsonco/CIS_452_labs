c4c33ae98014fae7f91b4d4e49e344faa0cb56d4
Larsonco <c.j.larson34@gmail.com>
2020-01-22 23:55:12 -0500

    adding comments

--- lab2/shell.c
+++ lab2/shell.c
@@ -1,3 +1,9 @@
+/***********************************************************************
+* Author:    Collin Larson & William Mckeever
+* Class:     CIS 241, Project 4
+* Due:       Jan 23rd, 2020
+* Description: 
+***********************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,31 +21,46 @@ void prompt();
 void parse_args();
 void execute();
 
+/** Initial command inputted by user */
 char inputCmd[MAX];
+
+/** Parsed command and args */
 char* inputArgs[MAX];
+
+/** Previous user cpu time */
 struct timeval oldTim;
+
+/** Command count */
 int cmdCount = 0;
 
+/** Main method ran for shell.c */
 int main(int argc, char **argv) {
   while (1) {
-    prompt();
+    prompt();                                 // display prompt to user
 
-    if (strncmp(inputCmd, "quit", 4) == 0)
-      break;
+    if (strncmp(inputCmd, "quit", 4) == 0)    // check if user wants to quit
+      break;                                  // break out of while loop
 
-    parse_args();
-    execute();
-    cmdCount += 1;
+    parse_args();                             // parse arguments from user
+    execute();                                // execute cmd given by user
+    cmdCount += 1;                            // increment # of commands ran
   } // end of while
 
-  return 0;
+  return 0;                                   // end program
 } // end of main
 
+/********************************************
+ * void function that displays a promt &
+ * gets user input
+ *******************************************/
 void prompt() {
   printf("Enter a command to execute (15 char max, quit to quit): ");
   fgets(inputCmd, MAX, stdin);
 }
 
+/********************************************
+ * void function that parses the user input
+ *******************************************/
 void parse_args() {
 
   char* delim = " \t\n";
@@ -59,11 +80,16 @@ void parse_args() {
 
 } // end of parse_args;
 
+/********************************************
+ * void function that executs the user input
+ * and display information about that
+ * command's run time & inv context switches
+ *******************************************/
 void execute() {
   pid_t pid;
   int status;
 
-  struct rusage usage;        // refer to man getrusage(2). Holds the current child proccess usage time.
+  struct rusage usage;                // refer to man getrusage(2). Holds the current child proccess usage time.
   struct timeval tim;                 // timeval struct
   long ivcsw;                         // involuntary context switches
 
@@ -71,10 +97,10 @@ void execute() {
     perror("fork failure");
     exit(1);
   } else if (pid == 0) {
-    execvp(inputArgs[0], inputArgs);
-    exit(0);
+    execvp(inputArgs[0], inputArgs);  // have child turn into the command to be executed
+    exit(0);                          // returning success, not checking for erros
   } else {
-    wait(&status);
+    wait(&status);                    // waiting for child to finish
     //getrusage(RUSAGE_SELF, &usage);
     getrusage(RUSAGE_CHILDREN, &usage);
     ivcsw = usage.ru_nivcsw;
@@ -82,8 +108,7 @@ void execute() {
 
     if (cmdCount == 0) {
       printf("Time elasped at: %ld.%lds\n", tim.tv_sec, tim.tv_usec);
-    } 
-    else {
+    } else {
        printf("Time elasped at: %ld.%lds\n", (tim.tv_sec-oldTim.tv_sec), (tim.tv_usec-oldTim.tv_usec));
     }
 
