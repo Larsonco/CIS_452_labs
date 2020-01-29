a85d05e745cb6fec9315d620bf4de7c68bb273c9
Larsonco <c.j.larson34@gmail.com>
2020-01-28 23:55:08 -0500

    comments

--- lab3/sig_project.c
+++ lab3/sig_project.c
@@ -1,3 +1,9 @@
+/***********************************************************************
+* Author:    Collin Larson & William Mckeever
+* Class:     CIS 452, Lab 3
+* Due:       Jan 30th, 2020
+* Description: Lab Programming Assignment (Communicating Processes)
+***********************************************************************/
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -7,34 +13,32 @@
 
 static int runningChild;
 
+/** Helper methods */
 void childProcess(void);
 void sigHandler1(int);
 void sigHandler2(int);
 void sigHandlerShutdown(int);
 
+/** Main method ran for sig_project.c */
 int main()
 {
     pid_t pid;
     int status;
 
+    /** SIGINIT is a signal from a keyboard */
     signal(SIGINT, sigHandlerShutdown);
 
-    if ((pid = fork()) < 0)             //Child Process
-    {
+    if ((pid = fork()) < 0) {
         perror("fork failure");
         exit(1);
-    } 
-    else if (pid == 0)                    //Parent Process
-    {              
+    } else if (pid == 0) {              
         childProcess();
-    } 
-    else 
-    {
-        wait(&status);                    //Waiting for child to finish
+    } else {
+        wait(&status);  
     }
 
     return 0;
-}
+}   // end of main
 
 void childProcess(void)
 {
@@ -43,6 +47,7 @@ void childProcess(void)
 
     runningChild = 1;
 
+    /** SIGUSER1 & 2 ared user defined signal 1 & 2*/
     signal(SIGUSR1, sigHandler1);
     signal(SIGUSR2, sigHandler2);
 
@@ -54,30 +59,23 @@ void childProcess(void)
         printf("waiting...\t");
         sleep(randTime);
 
-        if(randProcess == 1)
-        {
+        if (randProcess == 1) {
             raise(SIGUSR1);
-        }
-        else
-        {
+        } else {
             raise(SIGUSR2);
-        }
-    }
-}
+        }   // end of if
+    }   // end of while
+}   // end of childProcess
 
-void sigHandler1(int sigNum)
-{
+void sigHandler1(int sigNum) {
     printf(" received a SIGUSR1 signal.\n");
+}   // end of sigHandler1
 
-} 
-
-void sigHandler2(int sigNum)
-{
+void sigHandler2(int sigNum) {
     printf(" received a SIGUSR2 signal.\n");
-} 
+}   // end of sigHandler2
 
-void sigHandlerShutdown(int sigNum)
-{
+void sigHandlerShutdown(int sigNum) {
     printf(" received. That's it, I'm shutting you down...\n");
     runningChild = 0;
-} 
+}   // end of parent sigHandler
